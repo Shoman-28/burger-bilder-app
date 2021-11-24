0e2f2e93cdff245232c2bbef024ff08628d776db
shoman-28 <northstar9928@gmail.com>
2021-11-24 10:35:48 +0600

    user authcation

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-// connect MongoDB
-//Run Server
 const dotenv = require("dotenv");
 dotenv.config();
 const app = require("./app");
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGODB_SERVER, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(() => {
-    console.log("MongoDB Connection Failed!");
-  });
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+// mongoose
+//   .connect(process.env.MONGODB_SERVER, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch(() => {
+//     console.log("MongoDB Connection Failed!");
+//   });
 
 
   const port = process.env.PORT || 3002
